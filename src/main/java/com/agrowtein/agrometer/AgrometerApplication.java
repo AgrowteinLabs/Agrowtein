e86fade920ebaebb88793306729940aaf54ea0b4
shansreenivas <shan.sreenivas@volvocars.com>
2024-06-15 19:28:20 +0200

    initial project commit

---
+++ src/main/java/com/agrowtein/agrometer/AgrometerApplication.java
@@ -0,0 +1,13 @@
+package com.agrowtein.agrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrowtein/agrometer/AgrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrowtein.agrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
