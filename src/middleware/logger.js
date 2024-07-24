758c1a0ff6c304c4c4a8e595057c38f2114c9b9a
Avin Joshy <avinjoshy21@gmail.com>
2024-07-24 20:08:11 +0530

    Update logger.js

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -4,7 +4,13 @@ const path = require("path");
 
 const logger = (req, res) => {
   try {
-    const logFilePath = path.join(__dirname, "..", "logs", "access.log");
+    const logDir = path.join(__dirname, "..", "logs");
+    if (!fs.existsSync(logDir)) {
+      fs.mkdirSync(logDir, { recursive: true });
+    }
+    console.log(logDir)
+
+    const logFilePath = path.join(logDir, "access.log");
     const accessLogStream = fs.createWriteStream(logFilePath, { flags: "a" });
     const logFormat =
       `Date: :date[iso]\t:method\t:req[header="origin"]\t:url\tStatus: :status\t:response-time ms`;
