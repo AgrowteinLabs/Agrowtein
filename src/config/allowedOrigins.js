276b59861105340fbe9aa638abd7d144993d6d6c
Avin Joshy <avinjoshy21@gmail.com>
2024-08-10 19:25:37 +0530

    chore: Update allowedOrigins.js to include new origin URL

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:4500/",
   "https://admin-dashboard-theta-orpin.vercel.app/",
+  "https://admin-dashboard-9xxg0x2iz-abhinav-bavos-projects.vercel.app/"
 ];
 
 module.exports = allowedOrigins;
