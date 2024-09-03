59d5fcea396c3635e1c68a3069cdafa53356ca36
Avin Joshy <avinjoshy21@gmail.com>
2024-09-03 12:40:48 +0530

    chore: Update CORS configuration to include additional allowed origins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
   "http://localhost:4500/",
   "https://admin-dashboard-theta-orpin.vercel.app/",
-  "https://admin-dashboard-9xxg0x2iz-abhinav-bavos-projects.vercel.app/"
+  "https://admin-dashboard-9xxg0x2iz-abhinav-bavos-projects.vercel.app/",
+  "http://localhost:5173",
+  "https://agrowteinlabs.onrender.com",
 ];
 
 module.exports = allowedOrigins;
