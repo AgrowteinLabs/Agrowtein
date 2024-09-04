793f973b5ee619d0d9ada5f12c570bf2094fae0d
Avin Joshy <avinjoshy21@gmail.com>
2024-09-04 08:14:20 +0530

    chore: Update CORS configuration to remove unused middleware

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,14 +1,14 @@
-const allowedOrigins = require("./allowedOrigins");
+//const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-  origin: (origin, callback) => {
+  origin: true /*(origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       console.error(`CORS error: ${origin} not allowed by CORS`);
       callback(new Error("Not allowed by CORS"));
     }
-  },
+  }*/,
   optionsSuccessStatus: 200,
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
