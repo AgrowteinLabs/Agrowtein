7de64b0079cdcefc033005317e06ec8ce982886d
abhinav bavos <s4abhinav.19@gmail.com>
2025-05-16 18:20:24 +0530

    fix: update MongoDB connection settings and improve error handling

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", true, "useNewUrlParser", true);
-DB_URI = process.env.MONGODB_URI
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(DB_URI);
-  } catch (err) {
-    console.error(err.message);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
